1eff9a50511665d16e4eae8bb5400c7e811506f6
int55 <iga-pro555@bk.ru>
2020-03-04 16:02:55 +0300

    In file MainAtiity.java: System button BACK, double-clicking closes the application

--- app/src/main/java/com/example/android/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/android/webviewtest/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView web;
+    private long backPressedTime;
+    private Toast toastBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         web = (WebView) findViewById(R.id.webViewXML);
-        //переменная кот отвечает за настройки веб брауз
+        //переменная которая отвечает за настройки веб брауз
         WebSettings webSettings = web.getSettings();
-        //вкл js в браузере
+        //включает js в браузере
         webSettings.setJavaScriptEnabled(true);
-        //открывает сайт в нашем прил, а не в браузере по умолчанию
+        //открывает сайт в нашем приложении, а не в браузере по умолчанию
         web.setWebViewClient(new WebViewClient());
         //устан url
         web.loadUrl("https://www.yandex.ru");
@@ -30,8 +33,18 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         if(web.canGoBack()){
             web.goBack();
-        }else {
+        }else  //Системная кнопка "Назад". Двойное нажатие -закрытие приложения
+            if (backPressedTime + 2000 > System.currentTimeMillis()){
+            toastBack.cancel(); //закрыть Toast
+            //закрываем приложение
             super.onBackPressed();
+            return;
+        }else {
+
+            //Системная кнопка "Назад". Первое нажатие, всплыв подсказка
+            toastBack = Toast.makeText(getBaseContext(),"Нажмите еще раз чтобы выйти", Toast.LENGTH_SHORT);
+            toastBack.show(); // показать Toast
         }
+        backPressedTime = System.currentTimeMillis(); // получаем текущее время в мл/сек
     }
 }
